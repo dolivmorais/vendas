c1c287cf65a21074bfe8ecdbe575c0909df0f148
dolivmorais <dolivmorais@gmail.com>
2024-05-22 20:45:24 -0300

    commit criando opções de filtros

--- app.py
+++ app.py
@@ -15,14 +15,73 @@ server = app.server
 
 
 #======== sTYLES ===========
+tab_card = {'height': '100%'}
+
+main_config = {
+    "hovermode": "x unified",
+    "legend": {"yanchor": "top",
+               "y": 0.9,
+               "xanchor": "left",
+                "x": 0.1,
+                "title": {"text": None},
+                "font":  {"color": "white"},
+                "bgcolor": "rgba(0,0,0,0.5)"},
+    "margin": {"l": 10, "r": 10, "t": 10, "b": 10}
+}
+
+config_graph = {"displayModeBar": False, "showTips": False}
+
+template_theme1 = "flatly"
+template_theme2 = "darkly"
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.DARKLY
+
+#======== leitura csv ==========
+df = pd.read_csv("dataset.csv")
+df_cru = df.copy()
+
+# ====== meses em numeros
+df.loc[df['Mês'] == 'Jan', 'Mês'] = '01'
+df.loc[df['Mês'] == 'Fev', 'Mês'] = '02'
+df.loc[df['Mês'] == 'Mar', 'Mês'] = '03'
+df.loc[df['Mês'] == 'Abr', 'Mês'] = '04'
+df.loc[df['Mês'] == 'Mai', 'Mês'] = '05'
+df.loc[df['Mês'] == 'Jun', 'Mês'] = '06'
+df.loc[df['Mês'] == 'Jul', 'Mês'] = '07'
+df.loc[df['Mês'] == 'Ago', 'Mês'] = '08'
+df.loc[df['Mês'] == 'Set', 'Mês'] = '09'
+df.loc[df['Mês'] == 'Out', 'Mês'] = '10'
+df.loc[df['Mês'] == 'Nov', 'Mês'] = '11'
+df.loc[df['Mês'] == 'Dez', 'Mês'] = '12'
+
+#======== limpeza de dados ==========
+df["Chamadas Realizadas"] = df['Chamadas Realizadas'].astype('int64')
+df["Dia"] = df['Dia'].astype('int64')
+df["Mês"] = df['Mês'].astype('int64')
+df["Valor Pago"] = df["Valor Pago"].str.lstrip('R$ ')
+df['Valor Pago'] = df['Valor Pago'].astype('float64')
+df.loc[df['Status de Pagamento'] == 'Pago', 'Status de Pagamento'] = '1'
+df.loc[df['Status de Pagamento'] == 'Não pago', 'Status de Pagamento'] = '0'
+df['Status de Pagamento'] = df['Status de Pagamento'].astype('int64')
+
+
+
+
+#criando opções pros filtros
+options_month = [{'label': 'Ano todo', 'Value': 0}]
+for i, j in zip(df_cru["Mês"].unique(), df["Mês"].unique()):
+    options_month.append({'label': i, 'Value': j})
+options_month = sorted(options_month, key=lambda x: x['Value'])
+
+options_team = [{'label': 'Toda Equipe', 'Value': 0}]
+for i in df["Equipe"].unique():
+    options_team.append({'label': i, 'Value': i})
 
 
 #======== layout ===========
 app.layout = dbc.Container(children=[
 
     ], fluid=True, style={'height': '100vh'})
-
-
 
 #======== callbacks ========
 

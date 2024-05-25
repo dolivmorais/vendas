955173fede75199becab26b4fd77c4e515efe9f1
dolivmorais <dolivmorais@gmail.com>
2024-05-25 11:40:10 -0300

    adicionando segunda linha no layout

--- app.py
+++ app.py
@@ -142,7 +142,69 @@ app.layout = dbc.Container(children=[
                 ])
             ], style=tab_card)
         ], sm=12, lg=3)
-    ], className='g-2 my-auto', style={'margin-top': '7px'})
+    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+    
+    #linhha 2
+
+    dbc.Row([
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph3', className='dbc', config=config_graph)
+                        ])
+                    ], style=tab_card)
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph4', className='dbc', config=config_graph)
+                        ])
+                    ], style=tab_card)
+                ])
+            ], className='g-2 my-auto', style={'margin-top': '7px'})
+        ], sm=12, lg=5),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph5', className='dbc', config=config_graph)    
+                        ])
+                    ], style=tab_card)
+                ], sm=6),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph6', className='dbc', config=config_graph)    
+                        ])
+                    ], style=tab_card)
+                ], sm=6)
+            ], className='g-2'),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dcc.Graph(id='graph7', className='dbc', config=config_graph)
+                    ], style=tab_card)
+                ])
+            ], className='g-2 my-auto', style={'margin-top': '7px'})
+        ], sm=12, lg=4),
+        dbc.Col([
+            dbc.Card([
+                dcc.Graph(id='graph8', className='dbc', config=config_graph)
+            ], style=tab_card)
+        ], sm=12, lg=3)
+    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+
+    #linhha 3
+    dbc.Col([
+        
+    ])
+        
+
     ], fluid=True, style={'height': '100vh'})
 
 
